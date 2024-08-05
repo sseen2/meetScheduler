6d0a47696a10ed7916119e6c913b8ed6b0ac7c96
sseen2 <kse10023@naver.com>
2024-08-05 17:24:43 +0900

    project build

---
+++ src/main/java/js/meetScheduler/MeetSchedulerApplication.java
@@ -0,0 +1,13 @@
+package js.meetScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/js/meetScheduler/MeetSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package js.meetScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
